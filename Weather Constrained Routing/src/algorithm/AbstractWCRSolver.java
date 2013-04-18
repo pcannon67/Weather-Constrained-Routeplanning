47e1aa96e3183d046f56be24e47eadd24c8cd4e2
Zakaria Meziane <zmeziane@zmeziane-PC>
2013-04-18 20:50:40 +0200

    -

--- Weather Constrained Routing/src/algorithm/AbstractWCRSolver.java
+++ Weather Constrained Routing/src/algorithm/AbstractWCRSolver.java
@@ -5,7 +5,6 @@ import models.Graph;
 public abstract class AbstractWCRSolver implements PathFinder {
 	
 	protected Graph graph;
-
 	public AbstractWCRSolver(Graph graph) {
 		this.graph = graph;
 	}
